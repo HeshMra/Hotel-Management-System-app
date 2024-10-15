0e217b43dd5d580448db68f130d55f78bdb27e33
heshith <heshith619@gmail.com>
2024-10-15 16:47:22 +0530

    navbar creted'

--- Hotelapp/src/app/app.component.ts
+++ Hotelapp/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from '../Components/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
