830b3ca5e182cffff564e6ef79385390a3713afb
heshith <heshith619@gmail.com>
2024-10-16 09:00:04 +0530

    Admin and user dashboard link showing fixed based on the login

--- Hotelapp/src/Components/navigation-bar/navigation-bar.component.ts
+++ Hotelapp/src/Components/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../Services/AuthServices/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
+  constructor(public authservice:AuthService) { }
+
+
 
 }
